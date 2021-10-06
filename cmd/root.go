af95095ea380243f9b332ed06bd645cc302b3b13
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-06 08:51:41 -0500

    :recycle: Modularize the code

--- cmd/root.go
+++ cmd/root.go
@@ -1,56 +1,15 @@
 package cmd
 
-import (
-	"bufio"
-	"fmt"
-	. "github.com/UltiRequiem/chigo/pkg"
-	"io"
-	"os"
-	"strings"
-)
-
-func startProcessFromStdin() {
-	reader := bufio.NewReader(os.Stdin)
-
-	var j int = 1
-
-	for {
-		input, _, err := reader.ReadRune()
-
-		if err != nil && err == io.EOF {
-			break
-		}
-
-		r, g, b := GetRGB(j)
-
-		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, input)
-
-		j++
-	}
-}
-
 func Main() {
-	help, isTherFiles, files := getParametersAndFlags()
+	help, isThereFileArguments, files := getParametersAndFlags()
 
 	if help {
 		PrintHelp()
-                return
+		return
 	}
 
-	if isTherFiles {
-		text := JoinFilesToString(files)
-		scanner := bufio.NewScanner(strings.NewReader(text))
-
-		var j int = 1
-
-		for scanner.Scan() {
-			r, g, b := GetRGB(j)
-
-			fmt.Printf("\033[38;2;%d;%d;%dm%s\033[0m\n", r, g, b, scanner.Text())
-
-			j++
-		}
-
+	if isThereFileArguments {
+		PrintFilesWithScanner(files)
 		return
 	}
 
--- cmd/utils.go
+++ cmd/utils.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
+
+	. "github.com/UltiRequiem/chigo/pkg"
 )
 
 func JoinFilesToString(files []string) string {
@@ -21,9 +25,47 @@ func JoinFilesToString(files []string) string {
 		text[index] = string(fileText)
 	}
 
-	return strings.Join(text, "")
+	return strings.Join(text, "\n")
 }
 
 func PrintHelp() {
 	fmt.Println("TODO")
 }
+
+func PrintFilesWithScanner(files []string) {
+	text := JoinFilesToString(files)
+	scanner := bufio.NewScanner(strings.NewReader(text))
+
+	var j int = 1
+
+	for scanner.Scan() {
+		r, g, b := GetRGB(j)
+
+		fmt.Printf("\033[38;2;%d;%d;%dm%s\033[0m\n", r, g, b, scanner.Text())
+
+		j++
+	}
+}
+
+
+func startProcessFromStdin() {
+	reader := bufio.NewReader(os.Stdin)
+
+	var j int = 1
+
+	for {
+		input, _, err := reader.ReadRune()
+
+		if err != nil && err == io.EOF {
+			break
+		}
+
+		r, g, b := GetRGB(j)
+
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, input)
+
+		j++
+	}
+}
+
+
