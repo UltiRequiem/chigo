6271cc72173e56f1c5f8aa2bd4fb9b8ec01905aa
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-02 12:20:59 -0500

    :recycle: Remove extra spaces

--- cmd/parameters.go
+++ cmd/parameters.go
@@ -24,16 +24,15 @@ func printHelp() {
 
 const VERSION = "1.0.0"
 
-const HELP_MESSAGE = ` Chigo %s
-
- Concatenate FILE(s) or standard input to standard output.
- When no FILE is passed read standard input.
-  
-  Examples:
-    chigo fOne fTwo           # Output fOne and fTwo contents.
-    chigo                     # Copy standard input to standard output.
-    echo "My Message" | chigo # Display "My message".
-    fortune | chigo           # Display a rainbow cookie.
-            
- If you need more help, found a bug or want to suggest a new feature:
-  https://github.com/UltiRequiem/chigo`
+const HELP_MESSAGE = `Chigo %s
+Concatenate FILE(s) or standard input to standard output.
+When no FILE is passed read standard input.
+ 
+ Examples:
+   chigo fOne fTwo           # Output fOne and fTwo contents.
+   chigo                     # Copy standard input to standard output.
+   echo "My Message" | chigo # Display "My message".
+   fortune | chigo           # Display a rainbow cookie.
+           
+If you need more help, found a bug or want to suggest a new feature:
+https://github.com/UltiRequiem/chigo`
